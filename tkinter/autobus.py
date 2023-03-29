8ea2f7b3601921fbd8e147f84ed9095d51b438d2
Minex <107210625+yourdarl1ng@users.noreply.github.com>
2023-03-29 18:59:04 +0200

    Update autobus.py

--- tkinter/autobus.py
+++ tkinter/autobus.py
@@ -1,10 +1,20 @@
-
 import tkinter
-import random as r
+
 
 #vytvorenie kanvasu
 c = tkinter.Canvas()
-
+#predny vagon abo co , idk zevraj to mam mat >~<
+def predny_vagon():
+    for i in range(15):
+        posun = 3*i
+        c.create_line(30+posun, 0, 31+posun, 15)
+    c.create_rectangle(15, 0, 25, 20, fill="black")
+    c.create_rectangle(50, 40, 55, 45)
+    c.create_rectangle(0, 10, 95, 50, fill="red")
+    c.create_rectangle(65, 18, 95, 35, fill="green")
+    c.create_rectangle(50, 18, 90, 35, fill="green")
+    c.create_oval(20, 40, 40, 60, fill="black")
+    c.create_oval(50, 40, 70, 60, fill="black")
 #funkcia ktora zrobi autobus >w<
 def autobus(posun):
     #telo
@@ -16,8 +26,9 @@ def autobus(posun):
     #kolesa
     c.create_oval(115+posun, 40, 135+posun, 60, fill="black")
     c.create_oval(165+posun, 40, 185+posun, 60, fill="black")
-#chyba mi este predny vozen abo co
+
 #cyklus
+predny_vagon()
 for cyklus in range(5):
     separator = 105*cyklus
     autobus(separator)
