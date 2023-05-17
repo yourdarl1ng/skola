045ec66d70c00f6e10052f3f3118ce8f4415d8e3
Minex <107210625+yourdarl1ng@users.noreply.github.com>
2023-05-17 10:30:59 -0700

    Create lopta_2.py

---
+++ tkinter/lopta_2.py
@@ -0,0 +1,26 @@
+import tkinter
+canvas = tkinter.Canvas(width=400, height=400) 
+canvas.pack()
+x=15
+stop_var = False
+def stop(args):
+    global stop_var
+    if stop_var:
+        stop_var=False
+    else:
+        stop_var=True
+def lopta():
+    
+    global x 
+    
+    if x >= 400:
+        x=0
+    if not stop_var:
+        canvas.delete("all")
+        x+=5 
+        canvas.create_oval(x, 180, x+40, 220)
+        canvas.create_oval(180, x, 220, x+40)
+
+    canvas.after(200, lopta)
+lopta()
+canvas.bind("<Button-1>", stop)
