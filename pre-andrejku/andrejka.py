d42b88a344ebbe650430d91349602ea13cf41469
Minex <107210625+yourdarl1ng@users.noreply.github.com>
2024-03-18 17:13:51 +0100

    Create andrejka.py

---
+++ pre-andrejku/andrejka.py
@@ -0,0 +1,18 @@
+string = "Pajton"
+# Do range funkcie musi ist vzdy cislo!
+# Dali sme string
+#for i in range(string):
+#    print(i)
+
+len("Python") # Mozeme dosadit aj premennu
+# Vypise pismenko podla indexu
+for i in range(len(string)):
+    print(string[i])
+# vypise rook, preskoci z
+r = "rozok"
+print(r[0:2]+r[3:5])
+# Syntax zapisu rezov
+#[START : STOP : STEP]
+print("ANDREJ"[:1:2])
+# vypise jer, pretoze -1 to otoci
+print("andrejka"[-3:-6:-1])
