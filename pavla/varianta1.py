9cbbf7ff1fa09f30587d5c770cca37dc144e863b
Minex <107210625+yourdarl1ng@users.noreply.github.com>
2024-04-22 15:07:38 +0200

    Update varianta1.py

--- pavla/varianta1.py
+++ pavla/varianta1.py
@@ -11,7 +11,7 @@ def funkcia(retazec):
     # Zaciatok slucky for
     # Zmeriame dlzku retazca a nasledne ideme cez indexy
     for pismenko in range(len(retazec)):
-        # Ak je dany index delitelny 3 a nieje 0 tak sa pozreme na dany index od konca
+        # Ak je dany index delitelny 2 a nieje 0 tak sa pozreme na dany index od konca
         if pismenko % 2 == 0 and pismenko != 0:
             # Ked sa nan pozreme(od konca preto -) tak ho pripiseme ku vystupu
             vystup+=retazec[-pismenko]
