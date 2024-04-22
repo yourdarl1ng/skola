f4d9b8ed661d0cef90bcdcbd3a0f42cc6e14f971
Minex <107210625+yourdarl1ng@users.noreply.github.com>
2024-04-22 15:39:36 +0200

    pre adku

---
+++ pre-andrejku/varianta-bez-komm.py
@@ -0,0 +1,13 @@
+veta = input("Zadaj vetu na prehod: ")
+
+def prehod(veta2):
+
+    vystup = ""
+
+    for slovo in veta2.split():
+        vystup += slovo[::-1] + " "
+
+    return vystup
+
+vystup_funkcie = prehod(veta)
+print(vystup_funkcie)
